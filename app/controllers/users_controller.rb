3cb9abf957683abc6551068eb9daa93e437254b1
paul_vonhagen <pvonhagen26@gmail.com>
2020-11-24 18:26:06 -0800

    Set up users controller with index, create, show, and params

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class Api::UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: UserSerializer.new(users)
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: UserSerializer.new(user), status: :accepted
+        else
+            render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: UserSerializer.new(user)
+    end
+
+    def destroy
+        user = User.find_by(id: params[:id])
+        user.destroy
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password, :email, :location)
+    end
+    
+end
