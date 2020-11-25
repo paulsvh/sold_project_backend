aee4f8d206cf4103b2530d419d9fcb689c283382
paul_vonhagen <pvonhagen26@gmail.com>
2020-11-24 19:10:14 -0800

    installed and migrated ActiveStorage for image storage/processing

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
     belongs_to :user
-    has_one_attached :image
+    has_many_attached :image
     validates :title, presence: true
     validates :condition, presence: true
 end
