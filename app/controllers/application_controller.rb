9a5b184b5059c406893c31df73cbc2dae1859065
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-01 21:16:09 -0800

    removed helpers method from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::API
-    helpers do
         #spoofed current user method to send a user to frontend
         def current_user
             User.first
