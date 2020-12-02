c4d2f3efd1fc14dbc77e46a0fba51c7888171c80
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-01 21:11:10 -0800

    added logged_in? boolean to check if user is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,9 @@ class ApplicationController < ActionController::API
         def current_user
             User.first
         end
+
+        def logged_in?
+            !!current_user
+        end
     end
 end
