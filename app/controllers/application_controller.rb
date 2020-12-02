67ae088b6a0c00a9270bdef869290dcc61997353
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-01 21:10:05 -0800

    spoofed a helper method to send User.first as current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
-
+    helpers do
+        #spoofed current user method to send a user to frontend
+        def current_user
+            User.first
+        end
+    end
 end
