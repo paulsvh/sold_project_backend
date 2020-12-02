2b7d2377933fadb41ddd5d5179033e595037a6b1
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-01 20:49:35 -0800

    updated formatting on seed data, items are now created by their owner

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,19 @@
-User.create(username: "Paul", email: 'Paul@Paul.com', password: 'Paul', location: 'Outer Space')
+paul = User.create(username: "Paul", email: 'Paul@Paul.com', password: 'Paul', location: 'Outer Space')
 
-User.create(username: "Roanne", email: 'Roanne@Roanne.com', password: 'Roanne', location: 'The Desert')
+roanne = User.create(username: "Roanne", email: 'Roanne@Roanne.com', password: 'Roanne', location: 'The Desert')
 
-User.create(username: "Russ", email: 'Russ@Russ.com', password: 'Russ', location: 'The Desert')
+russ = User.create(username: "Russ", email: 'Russ@Russ.com', password: 'Russ', location: 'The Desert')
 
-User.create(username: "Mark", email: 'Mark@mark.com', password: 'Mark', location: 'The Beach')
+mark = User.create(username: "Mark", email: 'Mark@mark.com', password: 'Mark', location: 'The Beach')
 
-User.create(username: "Becca", email: 'Becca@becca.com', password: 'Becca', location: 'The Beach')
+becca = User.create(username: "Becca", email: 'Becca@becca.com', password: 'Becca', location: 'The Beach')
 
-Item.create(title: '4k tv', description: '50 inch 4k tv', condition: 'Great', value: '$400', user_id: 1)
+tv = paul.items.create(title: '4k tv', description: '50 inch 4k tv', condition: 'Great', value: '$400')
 
-Item.create(title: 'PS5', description: 'Brand new unopened PS5', condition: 'Mint', value: '$1200', user_id: 2)
+ps5 = roanne.items.create(title: 'PS5', description: 'Brand new unopened PS5', condition: 'Mint', value: '$1200',)
 
-Item.create(title: 'Frank', description: '13 year old black pug, mostly blind, poops whenever/wherever he feels like it.', condition: 'Used', value: 'Priceless tbh', user_id: 3)
+frank = russ.items.create(title: 'Frank', description: '13 year old black pug, mostly blind, poops whenever/wherever he feels like it.', condition: 'Used', value: 'Priceless tbh')
 
-Item.create(title: 'Enterprise model replica', description: 'Exact 1/10 scale replica of the starship Enterprise', condition: 'Pristine', value: '$3000', user_id: 4)
+enterprise = mark.items.create(title: 'Enterprise model replica', description: 'Exact 1/10 scale replica of the starship Enterprise', condition: 'Pristine', value: '$3000')
 
-Item.create(title: 'Masters Degree', description: 'Long and arduous masters degree in english literature, because thats totally relevant.', condition: 'New', value: '$120000', user_id: 5)
-
+degree = becca.items.create(title: 'Masters Degree', description: 'Long and arduous masters degree in english literature, because thats totally relevant.', condition: 'New', value: '$120000')
