587fc1c370c37eb166bcb13e8ecf4dc5f5ca8250
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-01 18:55:41 -0800

    Populated my seed file for proper db seeding. Built a rake task -rake db:dcm-s that drops the current database, creates a new one, runs migrates, and seeds from seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Paul", email: 'Paul@Paul.com', password: 'Paul', location: 'Outer Space')
+
+User.create(username: "Roanne", email: 'Roanne@Roanne.com', password: 'Roanne', location: 'The Desert')
+
+User.create(username: "Russ", email: 'Russ@Russ.com', password: 'Russ', location: 'The Desert')
+
+User.create(username: "Mark", email: 'Mark@mark.com', password: 'Mark', location: 'The Beach')
+
+User.create(username: "Becca", email: 'Becca@becca.com', password: 'Becca', location: 'The Beach')
+
+Item.create(title: '4k tv', description: '50 inch 4k tv', condition: 'Great', value: '$400', user_id: 1)
+
+Item.create(title: 'PS5', description: 'Brand new unopened PS5', condition: 'Mint', value: '$1200', user_id: 2)
+
+Item.create(title: 'Frank', description: '13 year old black pug, mostly blind, poops whenever/wherever he feels like it.', condition: 'Used', value: 'Priceless tbh', user_id: 3)
+
+Item.create(title: 'Enterprise model replica', description: 'Exact 1/10 scale replica of the starship Enterprise', condition: 'Pristine', value: '$3000', user_id: 4)
+
+Item.create(title: 'Masters Degree', description: 'Long and arduous masters degree in english literature, because thats totally relevant.', condition: 'New', value: '$120000', user_id: 5)
+
---
+++ lib/tasks/dcms.rake
@@ -0,0 +1,17 @@
+namespace :db do
+    desc 'Drop, Create, Migrate, Seed db, Start rails server'
+    task dcms: :environment do
+      puts 'dropping db....'
+      Rake::Task['db:drop'].invoke
+  
+      puts 'creating db....'
+      Rake::Task['db:create'].invoke
+  
+      puts 'running migrations ....'
+      Rake::Task['db:migrate'].invoke
+  
+      puts 'seeding db ....'
+      Rake::Task['db:seed'].invoke
+  
+    end
+  end
