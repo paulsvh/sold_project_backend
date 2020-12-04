cf82e513b80eb96873768e497b44617470e44d04
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-03 18:13:04 -0800

    attached status code to destroy function in sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -26,7 +26,7 @@ class Api::SessionsController < ApplicationController
         session.clear
         render json: {
             notice: "You have logged out"
-        }
+        }, status: :ok
     end
 
 end
