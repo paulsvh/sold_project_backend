eb0d68e93f73b456191f0c87ecde71f1ce9e227a
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-03 17:39:53 -0800

    added logout route to sessions controller delete function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 get '/api/get_current_user', to: 'api/sessions#get_current_user'
 post '/api/login', to: 'api/sessions#create'
+delete '/api/logout', to: 'api/sessions#destroy'
 namespace :api do
   resources :users, only: [:index, :create, :show]
   resources :items
