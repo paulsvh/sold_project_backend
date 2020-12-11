7b257832675e9e71ef0c163692e7a3b7a2b1ebde
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-10 17:22:32 -0800

    add signup route at users#create. create also sets the session user id to the new users id.

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,6 +8,7 @@ class Api::UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
+            session[:user_id] = user.id
             render json: user, status: :created, location: user
         else
             render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
@@ -22,7 +23,7 @@ class Api::UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password_digest, :email, :location)
+        params.require(:user).permit(:username, :password, :email, :location)
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 get '/api/get_current_user', to: 'api/sessions#get_current_user'
 post '/api/login', to: 'api/sessions#create'
 delete '/api/logout', to: 'api/sessions#destroy'
+post '/api/signup', to: 'api/users#create'
 namespace :api do
   resources :users, only: [:index, :create, :show]
   resources :items
