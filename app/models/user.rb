da0869bd3e20f4ef3bd7f1e42e3708c998eb9a1e
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-10 16:48:24 -0800

    add validation to user model to ensure presence of a location attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_many :items
     validates :username, uniqueness: true, presence: true
     validates :email, uniqueness: true, presence: true
+    validates :location, presence: true
     has_secure_password
 end
