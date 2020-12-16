9038e5018fb4ec09736635a20242b9ff6e775e1a
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-15 20:30:50 -0800

    adjust item model to reflect one attached image instead of many

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
     belongs_to :user
-    has_many_attached :image
+    has_one_attached :image
     validates :title, presence: true
     validates :condition, presence: true
 end
