bd3416e70052c8037939034d1642d63d69491048
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-16 16:04:46 -0800

    update serializer to include rails url helpers. use url helpers to assign a url to an items attached image.

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,13 @@
 class ItemSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
   belongs_to :user
   attributes :title, :description, :condition, :value, :image, :id
+
+  def image
+    if object.image.attached?
+      {
+        url: rails_blob_url(object.image)
+      }
+    end
+  end
 end
