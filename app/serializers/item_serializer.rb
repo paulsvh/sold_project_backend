bba99e2b0e71b335f4b1fabb8e1339393afb6c1a
paul_vonhagen <pvonhagen26@gmail.com>
2020-12-20 19:53:49 -0800

    add created_at attribute to items serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,7 +1,7 @@
 class ItemSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
   belongs_to :user
-  attributes :title, :description, :condition, :value, :image, :id
+  attributes :title, :description, :condition, :value, :image, :id, :created_at
 
   def image
     if object.image.attached?
